f77237c955edd15c3f75ec275e5368c92f8d0c96
Vinicius Pedrosa <vinicius.pedrosa@gmail.com>
2025-09-04 08:56:35 -0300

    Ajustes para mobile

--- resources/views/dividends/index.blade.php
+++ resources/views/dividends/index.blade.php
@@ -22,59 +22,59 @@
                             {{ session('success') }}
                         </div>
                     @endif
-
-                    <table class="min-w-full bg-white">
-                        <thead class="bg-gray-200">
-                            {{-- NOVA ORDEM DE COLUNAS NO CABEÇALHO --}}
-                            <tr>
-                                <th class="py-2 px-4 border-b">Data Com</th>
-                                <th class="py-2 px-4 border-b">Data Ex</th>
-                                <th class="py-2 px-4 border-b">Data Pag.</th>
-                                <th class="py-2 px-4 border-b">Tipo</th>
-                                <th class="py-2 px-4 border-b text-right">Valor por Cota (R$)</th>
-                                <th class="py-2 px-4 border-b text-right">Posição na Data</th>
-                                <th class="py-2 px-4 border-b text-right">Total Recebido (R$)</th>
-                                <th class="py-2 px-4 border-b">Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($dividends as $dividend)
-                                <tr class="hover:bg-gray-50">
-                                    {{-- NOVA ORDEM DAS CÉLULAS NO CORPO DA TABELA --}}
-                                    <td class="py-2 px-4 border-b text-center">{{ $dividend->record_date ? $dividend->record_date->format('d/m/Y') : 'N/A' }}</td>
-                                    <td class="py-2 px-4 border-b text-center">{{ $dividend->ex_date ? $dividend->ex_date->format('d/m/Y') : 'N/A' }}</td>
-                                    <td class="py-2 px-4 border-b text-center">{{ $dividend->payment_date ? $dividend->payment_date->format('d/m/Y') : 'N/A' }}</td>
-                                    <td class="py-2 px-4 border-b text-center font-semibold">{{ $dividend->type }}</td>
-                                    <td class="py-2 px-4 border-b text-right">{{ number_format($dividend->amount_per_share, 4, ',', '.') }}</td>
-                                    <td class="py-2 px-4 border-b text-right">{{ number_format($dividend->position_on_ex_date, 2, ',', '.') }}</td>
-                                    <td class="py-2 px-4 border-b text-right font-bold text-green-600">
-                                        R$ {{ number_format($dividend->total_received, 2, ',', '.') }}
-                                    </td>
-                                    
-                                    {{-- CÉLULA DE AÇÕES COM OS ÍCONES SVG CORRETOS --}}
-                                    <td class="py-2 px-4 border-b text-center whitespace-nowrap">
-                                        <div class="flex justify-center items-center">
-                                            <a href="{{ route('dividends.edit', $dividend) }}" title="Editar Provento" class="p-2 text-indigo-600 hover:text-indigo-900">
-                                                {{-- SVG do Heroicon 'Pencil Square' --}}
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg>
-                                            </a>
-                                            <form action="{{ route('dividends.destroy', $dividend) }}" method="POST" class="inline-block" onsubmit="return confirm('Tem certeza?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" title="Excluir Provento" class="p-2 text-red-600 hover:text-red-900">
-                                                    {{-- SVG do Heroicon 'Trash' --}}
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.033-2.134H8.033C6.91 2.75 6 3.664 6 4.834v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </td>
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full bg-white">
+                            <thead class="bg-gray-200">
+                                {{-- NOVA ORDEM DE COLUNAS NO CABEÇALHO --}}
+                                <tr>
+                                    <th class="py-2 px-4 border-b">Data Com</th>
+                                    <th class="py-2 px-4 border-b">Data Ex</th>
+                                    <th class="py-2 px-4 border-b">Data Pag.</th>
+                                    <th class="py-2 px-4 border-b">Tipo</th>
+                                    <th class="py-2 px-4 border-b text-right">Valor por Cota (R$)</th>
+                                    <th class="py-2 px-4 border-b text-right">Posição na Data</th>
+                                    <th class="py-2 px-4 border-b text-right">Total Recebido (R$)</th>
+                                    <th class="py-2 px-4 border-b">Ações</th>
                                 </tr>
-                            @empty
-                                <tr><td colspan="8" class="py-4 px-4 text-center text-gray-500">Nenhum provento registrado.</td></tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                    
+                            </thead>
+                            <tbody>
+                                @forelse ($dividends as $dividend)
+                                    <tr class="hover:bg-gray-50">
+                                        {{-- NOVA ORDEM DAS CÉLULAS NO CORPO DA TABELA --}}
+                                        <td class="py-2 px-4 border-b text-center">{{ $dividend->record_date ? $dividend->record_date->format('d/m/Y') : 'N/A' }}</td>
+                                        <td class="py-2 px-4 border-b text-center">{{ $dividend->ex_date ? $dividend->ex_date->format('d/m/Y') : 'N/A' }}</td>
+                                        <td class="py-2 px-4 border-b text-center">{{ $dividend->payment_date ? $dividend->payment_date->format('d/m/Y') : 'N/A' }}</td>
+                                        <td class="py-2 px-4 border-b text-center font-semibold">{{ $dividend->type }}</td>
+                                        <td class="py-2 px-4 border-b text-right">{{ number_format($dividend->amount_per_share, 4, ',', '.') }}</td>
+                                        <td class="py-2 px-4 border-b text-right">{{ number_format($dividend->position_on_ex_date, 2, ',', '.') }}</td>
+                                        <td class="py-2 px-4 border-b text-right font-bold text-green-600">
+                                            R$ {{ number_format($dividend->total_received, 2, ',', '.') }}
+                                        </td>
+                                        
+                                        {{-- CÉLULA DE AÇÕES COM OS ÍCONES SVG CORRETOS --}}
+                                        <td class="py-2 px-4 border-b text-center whitespace-nowrap">
+                                            <div class="flex justify-center items-center">
+                                                <a href="{{ route('dividends.edit', $dividend) }}" title="Editar Provento" class="p-2 text-indigo-600 hover:text-indigo-900">
+                                                    {{-- SVG do Heroicon 'Pencil Square' --}}
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg>
+                                                </a>
+                                                <form action="{{ route('dividends.destroy', $dividend) }}" method="POST" class="inline-block" onsubmit="return confirm('Tem certeza?');">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" title="Excluir Provento" class="p-2 text-red-600 hover:text-red-900">
+                                                        {{-- SVG do Heroicon 'Trash' --}}
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.033-2.134H8.033C6.91 2.75 6 3.664 6 4.834v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr><td colspan="8" class="py-4 px-4 text-center text-gray-500">Nenhum provento registrado.</td></tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/trades/closed.blade.php
+++ resources/views/trades/closed.blade.php
@@ -9,74 +9,74 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-
-                    <table class="min-w-full bg-white text-sm">
-                        <thead class="bg-gray-200">
-                            <tr>
-                                <th class="py-2 px-3 border-b text-left">Ativo</th>
-                                <th class="py-2 px-3 border-b text-right">Preço Médio Custo (R$)</th>
-                                <th class="py-2 px-3 border-b text-right">Preço Médio Venda (R$)</th>
-                                <th class="py-2 px-3 border-b text-right">Lucro / Prejuízo Realizado (R$)</th>
-                                <th class="py-2 px-3 border-b text-center">Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody class="divide-y">
-                            @forelse ($closedAssets as $asset)
-                                <tr class="hover:bg-gray-50">
-                                    <td class="py-2 px-3 border-b font-mono">
-                                        <div class="flex items-center">
-                                            @if ($asset->logo_path)
-                                                <img src="{{ $asset->logo_path }}" alt="Logo de {{ $asset->ticker }}" class="w-6 h-6 mr-3 rounded-full">
-                                            @else
-                                                <div class="w-6 h-6 mr-3 rounded-full bg-gray-200 flex items-center justify-center text-xs font-bold text-gray-500">{{ substr($asset->ticker, 0, 2) }}</div>
-                                            @endif
-                                            <span>{{ $asset->ticker }}</span>
-                                        </div>
-                                    </td>
-                                    {{-- NOVA COLUNA: Preço Médio Custo --}}
-                                    <td class="py-2 px-3 border-b text-right">
-                                        {{ number_format($asset->historical_average_buy_price, 2, ',', '.') }}
-                                    </td>
-                                    {{-- NOVA COLUNA: Preço Médio Venda --}}
-                                    <td class="py-2 px-3 border-b text-right">
-                                        {{ number_format($asset->average_sell_price, 2, ',', '.') }}
-                                    </td>
-                                    @php
-                                        $plColor = $asset->realized_profit_loss >= 0 ? 'text-green-600' : 'text-red-600';
-                                    @endphp
-                                    <td class="py-2 px-3 border-b text-right font-semibold {{ $plColor }}">
-                                        {{ number_format($asset->realized_profit_loss, 2, ',', '.') }}
-                                    </td>
-                                    <td class="py-2 px-3 border-b text-center">
-                                        <form action="{{ route('assets.destroy', $asset) }}" method="POST" class="inline-block" onsubmit="return confirm('Tem certeza que deseja excluir permanentemente este ativo e todo o seu histórico? Esta ação não pode ser desfeita.');">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" title="Excluir Ativo Permanentemente" class="p-2 text-red-600 hover:text-red-900 transition duration-150 ease-in-out">
-                                                {{-- SVG do Heroicon 'Trash' --}}
-                                                <svg xmlns="http://www.w.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.033-2.134H8.033C6.91 2.75 6 3.664 6 4.834v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
-                                            </button>
-                                        </form>
-                                    </td>                                    
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full bg-white text-sm">
+                            <thead class="bg-gray-200">
+                                <tr>
+                                    <th class="py-2 px-3 border-b text-left">Ativo</th>
+                                    <th class="py-2 px-3 border-b text-right">Preço Médio Custo (R$)</th>
+                                    <th class="py-2 px-3 border-b text-right">Preço Médio Venda (R$)</th>
+                                    <th class="py-2 px-3 border-b text-right">Lucro / Prejuízo Realizado (R$)</th>
+                                    <th class="py-2 px-3 border-b text-center">Ações</th>
                                 </tr>
-                            @empty
-                                <tr><td colspan="5" class="py-4 text-center text-gray-500">Nenhum trade encerrado encontrado.</td></tr>
-                            @endforelse
-                        </tbody>
-                        {{-- NOVO RODAPÉ COM O SOMATÓRIO --}}
-                        <tfoot class="bg-gray-200 font-bold">
-                            <tr>
-                                <td colspan="4" class="py-3 px-4 text-right">Total Realizado:</td>
-                                @php
-                                    $grandTotalPl = $closedAssets->sum('realized_profit_loss');
-                                    $grandTotalPlColor = $grandTotalPl >= 0 ? 'text-green-700' : 'text-red-700';
-                                @endphp
-                                <td class="py-3 px-4 text-right text-lg {{ $grandTotalPlColor }}">
-                                    R$ {{ number_format($grandTotalPl, 2, ',', '.') }}
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                
+                            </thead>
+                            <tbody class="divide-y">
+                                @forelse ($closedAssets as $asset)
+                                    <tr class="hover:bg-gray-50">
+                                        <td class="py-2 px-3 border-b font-mono">
+                                            <div class="flex items-center">
+                                                @if ($asset->logo_path)
+                                                    <img src="{{ $asset->logo_path }}" alt="Logo de {{ $asset->ticker }}" class="w-6 h-6 mr-3 rounded-full">
+                                                @else
+                                                    <div class="w-6 h-6 mr-3 rounded-full bg-gray-200 flex items-center justify-center text-xs font-bold text-gray-500">{{ substr($asset->ticker, 0, 2) }}</div>
+                                                @endif
+                                                <span>{{ $asset->ticker }}</span>
+                                            </div>
+                                        </td>
+                                        {{-- NOVA COLUNA: Preço Médio Custo --}}
+                                        <td class="py-2 px-3 border-b text-right">
+                                            {{ number_format($asset->historical_average_buy_price, 2, ',', '.') }}
+                                        </td>
+                                        {{-- NOVA COLUNA: Preço Médio Venda --}}
+                                        <td class="py-2 px-3 border-b text-right">
+                                            {{ number_format($asset->average_sell_price, 2, ',', '.') }}
+                                        </td>
+                                        @php
+                                            $plColor = $asset->realized_profit_loss >= 0 ? 'text-green-600' : 'text-red-600';
+                                        @endphp
+                                        <td class="py-2 px-3 border-b text-right font-semibold {{ $plColor }}">
+                                            {{ number_format($asset->realized_profit_loss, 2, ',', '.') }}
+                                        </td>
+                                        <td class="py-2 px-3 border-b text-center">
+                                            <form action="{{ route('assets.destroy', $asset) }}" method="POST" class="inline-block" onsubmit="return confirm('Tem certeza que deseja excluir permanentemente este ativo e todo o seu histórico? Esta ação não pode ser desfeita.');">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" title="Excluir Ativo Permanentemente" class="p-2 text-red-600 hover:text-red-900 transition duration-150 ease-in-out">
+                                                    {{-- SVG do Heroicon 'Trash' --}}
+                                                    <svg xmlns="http://www.w.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.033-2.134H8.033C6.91 2.75 6 3.664 6 4.834v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
+                                                </button>
+                                            </form>
+                                        </td>                                    
+                                    </tr>
+                                @empty
+                                    <tr><td colspan="5" class="py-4 text-center text-gray-500">Nenhum trade encerrado encontrado.</td></tr>
+                                @endforelse
+                            </tbody>
+                            {{-- NOVO RODAPÉ COM O SOMATÓRIO --}}
+                            <tfoot class="bg-gray-200 font-bold">
+                                <tr>
+                                    <td colspan="4" class="py-3 px-4 text-right">Total Realizado:</td>
+                                    @php
+                                        $grandTotalPl = $closedAssets->sum('realized_profit_loss');
+                                        $grandTotalPlColor = $grandTotalPl >= 0 ? 'text-green-700' : 'text-red-700';
+                                    @endphp
+                                    <td class="py-3 px-4 text-right text-lg {{ $grandTotalPlColor }}">
+                                        R$ {{ number_format($grandTotalPl, 2, ',', '.') }}
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
